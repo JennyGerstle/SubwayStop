18fbd5bf959882358435c195007b65dc39e971f0
jennygerstle@gmail.com <jennygerstle@gmail.com>
2021-11-02 11:20:48 -0400

    added a working test

--- src/main/java/CloseCoordinateStations.java
+++ src/main/java/CloseCoordinateStations.java
@@ -3,22 +3,14 @@ import java.util.List;
 
 public class CloseCoordinateStations
 {
-    public List<StationInfo.Station> getClosestStation(List<Double> coordinates, StationInfo stations)
+    public StationInfo.Station getClosestStation(List<Double> coordinates, StationInfo stations)
     {
-        List<StationInfo.Station> closestStations = new ArrayList<>();
-        closestStations.add(stations.features.get(0));
-        closestStations.add(stations.features.get(1));
+        StationInfo.Station closestStations = stations.features.get(0);
         for(StationInfo.Station station : stations.features)
         {
-            if(closer(closestStations.get(0), station, coordinates) && !station.equals(closestStations.get(1)))
+            if(closer(closestStations, station, coordinates))
             {
-                closestStations.add(station);
-                closestStations.remove(0);
-            }
-            if(closer(closestStations.get(1), station, coordinates) && !station.equals(closestStations.get(0)))
-            {
-                closestStations.add(station);
-                closestStations.remove(1);
+                closestStations = station;
             }
         }
         return closestStations;
